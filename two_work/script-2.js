f31f3fec6ce5f85f56bfe4632deba977e525faaf
Oksana Ovchinnikova <oksana_ovchinnikova@inbox.ru>
2021-04-21 16:36:44 +0500

    create index-2.html (dop. lesson) and script-2

---
+++ two_work/script-2.js
@@ -0,0 +1,62 @@
+"Use srtict";
+const message = document.querySelector(".clock");
+
+function getDate(deadline) {
+  let date = new Date();
+
+  const dateD = date.getTime(),
+    dateStop = new Date(deadline).getTime(),
+    timeRemaining = (dateStop - dateD) / 1000,
+    seconds = date.getSeconds(),
+    minutes = date.getMinutes(),
+    hours = date.getHours(),
+    timeDays = timesDay(),
+    week = [
+      "Воскресенье",
+      "Понедельник",
+      "Вторник",
+      "Среда",
+      "Четверг",
+      "Пятница",
+      "Суббота",
+    ],
+    hepNewEar = Math.floor(timeRemaining / 60 / 60 / 24);
+
+  let day;
+  week.forEach((item, i) => {
+    if (date.getDay() === i) {
+      day = item;
+    }
+  });
+
+  function createZero(elem) {
+    if (elem < 10) {
+      elem = `0${elem}`;
+      return elem;
+    }
+    return elem;
+  }
+
+  function timesDay() {
+    if (hours >= 5 && hours < 13) {
+      return "Доброе утро";
+    } else if (hours >= 13 && hours <= 18) {
+      return "Добрый день";
+    } else if (hours > 18 && hours <= 22) {
+      return "Добрый вечер";
+    } else if (hours > 22 && hours < 5) {
+      return "Доброй ночи";
+    }
+  }
+
+  message.innerHTML = `
+    ${timeDays}<br>
+    Сегодня: ${day}<br>
+    Текущее время: ${createZero(hours)}:${createZero(minutes)}:${createZero(
+    seconds
+  )}<br>
+    До нового года осталось ${hepNewEar}<br>
+  `;
+}
+
+setInterval(getDate, 1000, "1 January 2022");
